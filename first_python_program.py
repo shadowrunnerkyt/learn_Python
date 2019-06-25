aa4073bbc9e91d2b0dcef4894fc35ad34115be09
Chris <shadowrunnerkyt@gmail.com>
2019-06-25 16:56:24 -0400

    first upload of tutorial doc

---
+++ first_python_program.py
@@ -0,0 +1,16 @@
+_testVar = 100
+_testVar2 = 500
+
+print(_testVar)
+print(_testVar2)
+
+num_1 = input("Please enter a number")
+num_2 = input("Please enter a second number")
+result = int(num_1) + int(num_2)
+
+def add(num1,num2):
+    return int(num_1) + int(num_2) 
+print("Your result was: " + str(result))
+print("Your add result was: " + str(add(num_1,num_2)))
+
+
