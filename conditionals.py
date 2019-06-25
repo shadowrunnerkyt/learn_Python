c4d11dc203e072199ec2d44e7ebd385a942a14ad
Chris <shadowrunnerkyt@gmail.com>
2019-06-25 17:42:42 -0400

    a few changes

---
+++ conditionals.py
@@ -0,0 +1,13 @@
+
+x=10
+y=20
+
+if x==y:
+    print("This print statement should never be run")
+else:
+    print("x is NOT equal to y")
+
+if y>x:
+    print("Y is greater than x")
+else:
+    print("this print statement also, should never run")
