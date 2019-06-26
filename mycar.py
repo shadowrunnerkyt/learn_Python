a4b96977285a5a35a232a02bf6bfc12ab0a427a0
shadowrunnerkyt <shadowrunnerkyt@gmail.com>
2019-06-26 15:31:28 -0400

    class inheritence

--- mycar.py
+++ mycar.py
@@ -1,29 +1,44 @@
 
 
 class Car:
-    def __init__(self,color,model):
-        self.color = color
-        self.model = model
-        self.engine = "off"
+    def __init__(self):
+        self.engine = False
 
     def startEngine(self):
-        if self.engine == "off":
+        if self.engine == False :
             print("starting engine")
-            self.engine = "on"
+            self.engine = True
         else:
             print("the engine is already running")
+
     def stopEngine(self):
-        if self.engine == "on":
+        if self.engine == True:
             print("stopping engine")
-            self.engine = "off"
+            self.engine = False
         else:
             print("the engine is already off")
+
     def drive(self):
-        if self.engine == "on":
+        if self.engine == True:
             print("driving the car!")
         else:
             print("the engine needs to be started first")
+
     def status(self):
         print("Color: " + self.color)
         print("Model: " + self.model)
-        print("The engine is " + self. engine)
+        print("The engine is " + str(self.engine))
+
+class BlueCar:
+    color = 'Blue'
+
+class Honda(Car, BlueCar):
+    make = "Honda"
+
+    def startEngine(self):
+        super().startEngine()
+        print("Starting the Honda engine!")
+
+class HondaCivic(Honda):
+    model = 'Civic'
+
