942873a76e312197d66c74853646d9277893f8ac
Chris <ec2-user@ip-172-31-86-178.ec2.internal>
2019-06-28 15:34:45 +0000

    exceptions part B

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,9 @@
 
-myarray = [1,2,3]
+myarray = {1,2,3}
 for item in [0,1,2,3,4,5]:
-    print(myarray[item])
+    try:
+        print(myarray[item])
+    except IndexError:
+        print("Failed at index " + str(item))
 
+print("The program continues to run!")
